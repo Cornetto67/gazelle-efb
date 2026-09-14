dd948b2daaa45a91dffce6707be50783287e0d92
Guillaume <guill@example.com>
2026-09-14 21:35:49 +0200

    Forcer l'affichage des limites GTM en tête de liste dans l'onglet Performances

--- app.js
+++ app.js
@@ -667,3 +667,4 @@ function renderGtmTableHtml(chartDef, plotDiv) {
 
     plotDiv.innerHTML = buildTableHTML("COUPLE MAXI 5 mn (Décollage)", matDec, fullDec) + buildTableHTML("COUPLE MAXI CONTINU", matCont, fullCont);
 }
+
